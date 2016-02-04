61b91347a653dcf3c220538c6e945f44ea2a2a4e
Deepak Sharma <dsharm10@156TR4160MP-02.campus.MCGILL.CA>
2016-02-03 23:02:42 -0500

    Commit before adding custom scatter filw

--- Base_prj/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
+++ Base_prj/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
@@ -185,6 +185,12 @@ __Vectors_Size  EQU  __Vectors_End - __Vectors
 Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
 								 IMPORT  example1
+								 LDR.W R0, =0xE000ED88
+								 LDR R1, [R0]
+								 ORR R1, R1, #(0xF << 20)
+								 STR R1, [R0]
+								 DSB
+								 ISB
 								 LDR 		 R0, =example1
                  BLX     R0
 				 NOP
--- Base_prj/data.s
+++ Base_prj/data.s
@@ -1,7 +1,4 @@
 	AREA myData, DATA, READWRITE
-	EXPORT mystring1
-	EXPORT mystring2
-mystate DCW 1, 1, 1, 0, 0
-myinput DCW 
-myoutput DCW 0
+	EXPORT myoutput
+myoutput DCFS 0.0, 0.0, 0.0, 0.0
 	END
