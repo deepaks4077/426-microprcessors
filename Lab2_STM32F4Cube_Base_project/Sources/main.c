93dc97926bd4cdff0cc34ba2d0f03a1bd5e0f9fd
Deepak Sharma <dsharm10@156TR4160MP-15.campus.MCGILL.CA>
2016-02-18 12:47:57 -0500

    Deleted previous project and renamed active project

--- Lab2_STM32F4Cube_Base_project/Sources/main.c
+++ Lab2_STM32F4Cube_Base_project/Sources/main.c
@@ -14,29 +14,109 @@
 #include "supporting_functions.h"
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef ADC1_Handle;
+
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config	(void);
-GPIO_InitTypeDef* GPIOxInit(uint32_t pinNum, uint32_t mode, uint32_t pull, uint32_t speed, uint32_t alternate);
+void ADC_config(void);
+ADC_ChannelConfTypeDef ADC_Get_ChannelConfig(void);
+float Convert_Voltage_To_Temperature(uint32_t Voltage);
 
 int main(void)
 {
-  /* MCU Configuration----------------------------------------------------------*/
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
- 	GPIO_InitTypeDef* GPIO_A = GPIOxInit(GPIO_PIN_1, GPIO_MODE_INPUT, GPIO_PULLDOWN, GPIO_SPEED_FREQ_VERY_HIGH, GPIO_MODE_AF_PP);
 	
-	HAL_Init();
+	uint32_t Voltage;
+	float Temperature;
+	
+	/* MCU Configuration----------------------------------------------------------*/
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 	
   /* Configure the system clock */
   SystemClock_Config();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
 	
-	// Initialize GPIO_A Ports
-	HAL_GPIO_Init(GPIOA,GPIO_A);
-	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_1,GPIO_PIN_SET);
-	//while (1){
-			//GPIOA->BSRR = GPIO_PIN_SET;
-	//}
+	/* Configure the ADC clock --------------------------------------------------*/
+	__ADC1_CLK_ENABLE();
+	
+	/* Configure and start the ADC1 peripheral ---------------------------------------------*/
+	ADC_config();
+	
+	while (1){
+		Voltage = HAL_ADC_GetValue(&ADC1_Handle);
+		Temperature = Convert_Voltage_To_Temperature(Voltage);	
+		printf("TEMPERATURE %f <-------------------> VOLTAGE %d\n", Temperature, Voltage);
+	}
+}
+
+/* Convert the Voltage to Temperature ------------------------------------------*/
+float Convert_Voltage_To_Temperature(uint32_t Voltage){
+	
+	float Temperature = Voltage * 1.0f;
+	
+	Temperature *= 3000.0f/0xfff;
+	Temperature /= 1000;
+	Temperature -= 0.76;
+	Temperature /= 0.025;
+	Temperature += 25.0;
+	
+	return Temperature;
+}
+
+
+/** Configure and start ADC1 -------------------------------------------------------------*/
+void ADC_config(void){
+
+	ADC_InitTypeDef* ADC_Init;
+	ADC_ChannelConfTypeDef* ADC1_Channel;
+	
+	ADC1_Handle.Instance = ADC1;
+	ADC1_Handle.NbrOfCurrentConversionRank = 1;
+	ADC1_Handle.State = HAL_ADC_STATE_READY;
+	
+	/* Configure init handler ----------------------------------------------------*/
+	
+	ADC_Init->ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+	ADC_Init->Resolution = ADC_RESOLUTION12b;
+	ADC_Init->DataAlign = ADC_DATAALIGN_RIGHT;
+	ADC_Init->ScanConvMode = DISABLE;
+	ADC_Init->EOCSelection = ADC_EOC_SINGLE_CONV; // | ADC_EOC_SEQ_CONV | ADC_EOC_SINGLE_SEQ_CONV
+	ADC_Init->ContinuousConvMode = DISABLE;
+	ADC_Init->NbrOfConversion = 1;
+	ADC_Init->ExternalTrigConv = ADC_SOFTWARE_START;
+	ADC_Init->ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	
+	/* Add the init config handle to the ADC handler ----------------------------*/
+	
+	ADC1_Handle.Init = *ADC_Init;
+	
+	/* Configure the data acquisition channel -----------------------------------*/
+	
+	*ADC1_Channel = ADC_Get_ChannelConfig();
+	
+	HAL_ADC_ConfigChannel(&ADC1_Handle, ADC1_Channel);
+	
+	/* Initialize ADC1 -----------------------------------------------------------*/
+	if(HAL_ADC_Init(&ADC1_Handle) != HAL_OK){
+		Error_Handler(ADC_INIT_FAIL);
+	}
+	
+	/* Start the ADC -------------------------------------------------------------*/
+	HAL_ADC_Start(&ADC1_Handle);
+
+}
+
+/** Configure ADC1 Channel ---------------------------------------------------*/
+ADC_ChannelConfTypeDef ADC_Get_ChannelConfig(){
+	
+	ADC_ChannelConfTypeDef* ADC1_Channel;
+	
+	ADC1_Channel->Channel = ADC_CHANNEL_16; // Grab the Temperature channel
+	ADC1_Channel->Rank = 1;
+	ADC1_Channel->Offset = 0;
+	ADC1_Channel->SamplingTime = ADC_SAMPLETIME_480CYCLES;
+	
+	return *ADC1_Channel;
 }
 
 
@@ -77,23 +157,6 @@ void SystemClock_Config(void){
 
   /* SysTick_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-}
-
-/**
-	* @brief A function to return a configuration structure for a GPIO port
-	*	@retval None
-	*/
-
-GPIO_InitTypeDef* GPIOxInit(uint32_t pinNum, uint32_t mode, uint32_t pull, uint32_t speed, uint32_t alternate){
-	
-	GPIO_InitTypeDef* init;
-	init->Pin = pinNum;
-	init->Mode = mode;
-	init->Pull = pull;
-	init->Speed = speed;
-	init->Alternate = alternate;
-
-	return init;
 }
 
 /**
